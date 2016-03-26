27d46e98541774f0ff26218c56cf903eda10535a
Binara Darsha <wbdarsha@yahoo.com>
2016-03-26 20:18:34 +0530

    add function - printing sequence corrected

--- calc.py
+++ calc.py
@@ -1,10 +1,11 @@
 class Calc:
+
 	def add(self, x, y):
 		self.x = x
 		self.y = y
 		return x+y
 
 c = Calc()
-print("x = " + c.x)
-print("y = " + c.y)
-print(c.add(5, 7))
+print("x+y = " + str(c.add(5, 7)))
+print("x = " + str(c.x))
+print("y = " + str(c.y))
