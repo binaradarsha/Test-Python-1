72cf53438b786412138cddbe7785a1083a4248e7
Binara Darsha <wbdarsha@yahoo.com>
2016-05-01 20:33:26 +0800

    Substract function and printing added

--- calc.py
+++ calc.py
@@ -4,7 +4,17 @@ class Calc:
 		self.y = y
 		return x+y
 
+	def substract(self, x, y);
+		self.x = x
+		self.y = y
+		return x-y
+
 c = Calc()
+
 print("x+y = " + str(c.add(5, 7)))
+print("x = " + str(c.x))
+print("y = " + str(c.y))
+
+print("x-y = " + str(c.substract(10, 8)))
 print("x = " + str(c.x))
 print("y = " + str(c.y))
